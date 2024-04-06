3df74c9d241ab564956c2110c7eee198a69bbeb5
German Kosach <fuerstein@yandex.ru>
2024-04-06 20:59:16 +0300

    fix: namings inside of backend folder

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { createPublicClient, formatEther, http } from 'viem';
+import * as chains from 'viem/chains';
+import * as tokenJson from '../assets/MyToken.json';
+import * as process from 'process';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  publicClient;
+
+  constructor() {
+    const rpcEndpointUrl = process.env.RPC_ENDPOINT_URL;
+    const transport = http(rpcEndpointUrl);
+
+    this.publicClient = createPublicClient({
+      chain: chains.sepolia,
+      transport: transport,
+    });
+  }
+
+  async getTokenName(): Promise<any> {
+    return await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'name',
+    });
+  }
+
+  getContractAddress(): string {
+    return '0x8101b1ccc6829975e4ccf3a3525a689f4c564c72';
+  }
+
+  async getTotalSupply() {
+    const totalSupply = await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'totalSupply',
+    });
+    return formatEther(totalSupply as bigint);
+  }
+
+  async getTokenBalance(address: string) {
+    const totalSupply = await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'balanceOf',
+      args: [address],
+    });
+    return formatEther(totalSupply as bigint);
+  }
+
+  getTransactionReceipt(hash: string) {
+    const transactionReceipt = this.publicClient.getTransactionReceipt({
+      hash: hash,
+    });
+    console.log({ transactionReceipt });
+    return transactionReceipt;
   }
 }
